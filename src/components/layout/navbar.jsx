cc7d7f4b61f5ed3abe96f2f8e17f38f3df8ab308
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-29 17:13:24 +0530

    add marquee

--- src/components/layout/navbar.jsx
+++ src/components/layout/navbar.jsx
@@ -20,6 +20,21 @@ function Navbar() {
           <Button className="text-black"> दुकानें</Button>
         </Link>
       </nav>
+      <nav></nav>
+      <Divider />
+      <nav className="lg:px-10 px-4 py-2  md:flex hidden  flex-wrap  justify-between bg-yellow-300 fix ">
+        <marquee>
+          <p className="text-black font-mono tracking-widest">
+            || हमने अस्थायी रूप से वेबसाइट काम को वित्तीय संकट के कारण बंद कर
+            दिया है। हम आपकी सहनशीलता और समझ की कदर करते हैं। हम जल्द ही काम फिर
+            से शुरू करेंगे और इस वेबसाइट को पूरा करेंगे। आपके समर्थन के लिए
+            धन्यवाद। खुश रहें! Contact: 7800818001 || We have temporarily halted
+            our work due to financial constraints. We appreciate your patience
+            and understanding. We will resume work as soon as possible and
+            complete this website. Thank you for your support. Stay happy! ||
+          </p>
+        </marquee>
+      </nav>
       <Divider />
       <nav className="lg:px-10 px-4 py-2  md:flex hidden  flex-wrap  justify-between bg-black fix ">
         <div className="flex md:gap-8 gap-4 flex-wrap">
